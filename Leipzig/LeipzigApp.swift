947e01697ef3552fea5e8d3a96ccd425bbe1ed37
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-04-27 14:51:59 +0200

    Format files

--- Leipzig/LeipzigApp.swift
+++ Leipzig/LeipzigApp.swift
@@ -1,17 +1,10 @@
-//
-//  LeipzigApp.swift
-//  Leipzig
-//
-//  Created by Bryan Oppong-Boateng on 27.04.24.
-//
-
 import SwiftUI
 
 @main
 struct LeipzigApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
