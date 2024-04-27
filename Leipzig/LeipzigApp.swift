876404e96dc1807e2485256e4ff771f4b338d132
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-04-27 14:32:08 +0200

    Initial Commit

---
+++ Leipzig/LeipzigApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeipzigApp.swift
+//  Leipzig
+//
+//  Created by Bryan Oppong-Boateng on 27.04.24.
+//
+
+import SwiftUI
+
+@main
+struct LeipzigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
